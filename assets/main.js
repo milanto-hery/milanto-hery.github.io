95fb57d639b4441154d7e424b323d2d50db35438
Milanto <78157308+milanto-hery@users.noreply.github.com>
2026-01-29 11:27:42 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,14 @@
-// Floating shapes and reveal effect
-document.addEventListener("DOMContentLoaded", function() {
-    const reveals = document.querySelectorAll(".reveal");
-    function reveal() {
-        for (let i = 0; i < reveals.length; i++) {
-            const windowHeight = window.innerHeight;
-            const elementTop = reveals[i].getBoundingClientRect().top;
-            const elementVisible = 150;
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            }
-        }
+// Simple reveal animations
+window.addEventListener("scroll", function(){
+  const reveals = document.querySelectorAll(".reveal");
+  for(let i=0; i<reveals.length; i++){
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+    if(elementTop < windowHeight - revealPoint){
+      reveals[i].classList.add("active");
+    } else{
+      reveals[i].classList.remove("active");
     }
-    window.addEventListener("scroll", reveal);
-    reveal();
+  }
 });
